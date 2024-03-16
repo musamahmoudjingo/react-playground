6cb63201e9064818290e72930e448bd8dad1640c
musamahmoudjingo <musa@readdress.me>
2024-03-17 00:59:00 +0400

    Fault tolerance

--- src/app/fault-tolerance/page.tsx
+++ src/app/fault-tolerance/page.tsx
@@ -1,3 +1,26 @@
 export default function FaultTolerance() {
-  return <div>Fault Tolerance</div>
+  return (
+    <div>
+      <TopComponent />
+      <ErrorProneComponent />
+      <BottomComponent />
+    </div>
+  )
+}
+
+function TopComponent() {
+  return <h2>Top component</h2>
+}
+
+function BottomComponent() {
+  return <h2>Bottom component</h2>
+}
+
+function ErrorProneComponent() {
+  throw Error('oops, you broke it')
+  return <h2>Error prone component</h2>
+}
+
+function NiceErrorMessage() {
+  return <p>Something when wrong</p>
 }
